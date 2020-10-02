9f0c1a6b6b5dc7a004767a156dd57c6c890b40c6
Patrick Coxall <mr.coxall@mths.ca>
2020-10-02 08:00:07 -0700

    final code

--- main.ts
+++ main.ts
@@ -2,6 +2,10 @@
 // Created on: Sept 2020
 // 
 // This program lights up Neopixels on a Robot:Bit
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.clear()
+strip.show()
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 })
